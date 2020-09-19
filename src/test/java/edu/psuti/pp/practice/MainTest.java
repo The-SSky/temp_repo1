44f58038292e230ff242c4e1d5e9bdbc28c5d2cb
e.chasov <e.chasov@psuti.ru>
2020-09-19 14:52:21 +0400

    Initial commit

---
+++ src/test/java/edu/psuti/pp/practice/MainTest.java
@@ -0,0 +1,13 @@
+package edu.psuti.pp.practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        assertFalse(false);
+    }
+}
